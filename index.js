39788abebf443700220de4d47930391f2152fde0
Saksham Sengar <sakshamsengar479@gmail.com>
2025-02-19 03:39:03 +0530

    connect rename

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
-const mongooseConnectionHandler = require("./connect");
+const mongooseConnectionHandler = require("./connect.js");
 const URL = require("./models/url");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
